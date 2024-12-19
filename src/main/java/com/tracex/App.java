fc731f7d2311c9c680d9c03160d715ce8646f986
bhubany <yadav.bhuban.by@gmail.com>
2024-12-19 11:41:50 +0545

    chore(): initial setup

---
+++ src/main/java/com/tracex/App.java
@@ -0,0 +1,13 @@
+package com.tracex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
